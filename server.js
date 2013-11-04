6f5ce54cbd31713cfac98440329c8af824d45581
David Kua <david@kua.io>
2013-11-04 16:35:10 -0500

    Updated server.js

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
+// Dependencies
 var express = require("express");
 var orm = require("orm");
+var http = require("http");
 var app = express();
 
+// Variables
 var databaseFile = "database.sqlite3";
 var port = 41156;
 var address = "localhost";
+var vgOptions = {
+  host: "api.videogami.co",
+  port: 80
+};
 
+
+// Database Setup
 app.use(orm.express("sqlite://" + databaseFile, {
   define: function(db, models, next) {
     models.vote = db.define("vote", {
@@ -27,19 +36,25 @@ app.use(orm.express("sqlite://" + databaseFile, {
   }
 }));
 
+
+// Index
 app.get("/", function(req, res) {
   res.type("text/html");
   res.sendfile("./views/index.html");
 });
 
+
+// Clips
 app.get("/clips", function(req, res) {
-  res.type("text/json");
+
 });
 
 app.get("/clips/:id", function(req, res) {
   res.type("text/json");
 });
 
+
+// Upvotes
 app.get("/clips/:id/upvote", function(req, res) {
   req.models.vote.getVotes(req.params.id, 1, function(err, upvotes) {
     numOfUpvotes = { upvotes: upvotes.length }
@@ -54,6 +69,8 @@ app.post("/clips/:id/upvote", function(req, res) {
   });
 });
 
+
+// Downvotes
 app.get("/clips/:id/downvote", function(req, res) {
   req.models.vote.getVotes(req.params.id, -1, function(err, downvotes) {
     numOfDownvotes = { downvotes: downvotes.length }
@@ -68,5 +85,13 @@ app.post("/clips/:id/downvote", function(req, res) {
   });
 });
 
+
+// 404 Handler
+app.get("*", function(req, res) {
+  res.send("404 Not Found\n Whatttt?!?!", 404);
+});
+
+
+// Start server
 app.listen(port, address);
 console.log("Server running at %s:%d", address, port);
