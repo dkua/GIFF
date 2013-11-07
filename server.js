f44fc090d06c24e1904abdd2792ffc2452fa395a
David Kua <david@kua.io>
2013-11-07 13:50:26 -0500

    Finished voting with sessions system

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var express = require("express"),
     env = (function() {
       var Habitat = require("habitat");
-      Habitat.load;
+      Habitat.load();
       return new Habitat();
     }()),
     app = express();
@@ -10,50 +10,74 @@ var orm = require("orm");
 var http = require("http");
 var SQLiteStore = require("connect-sqlite3")(express);
 
+
+// Variables
+var databaseFile = "database.sqlite3";
+
+
 // Configurations
 app.configure(function() {
+  app.set("port", process.env.PORT || 41156);
   app.use(express.cookieParser());
   app.use(express.session({
     store: new SQLiteStore,
-    secret: env.get("SESSION_SECRET"),
+    secret: env.get("SECRET"),
     cookie: {
       maxAge: 2678400000 // 31 day limit
     }
   }));
+  // Database Setup
+  app.use(orm.express("sqlite://" + databaseFile, {
+    define: function(db, models, next) {
+      models.vote = db.define("vote", {
+        clipID: String,
+        value: Number,
+        sessionID: String
+      });
+      models.vote.getVote = function(clipID, sessionID, callback) {
+        this.find({ clipID: clipID, sessionID: sessionID }, callback);
+      };
+      models.vote.getAllVotes = function(clipID, value, callback) {
+        this.find({ clipID: clipID, value: value }, callback);
+      };
+      models.vote.setVote = function(clipID, value, sessionID, callback) {
+        this.create([{ clipID: clipID, value: value, sessionID: sessionID }], callback);
+      };
+
+      db.sync(function(err) {
+        if (err) throw err;
+        console.log("Database Synced");
+      });
+      next();
+    }
+  }));
+  app.use(app.router);
 });
 
 
-// Variables
-var databaseFile = "database.sqlite3";
-var port = 41156;
-var address = "localhost";
-var vgOptions = {
-  host: "api.videogami.co",
-  port: 80
-};
-
-
-// Database Setup
-app.use(orm.express("sqlite://" + databaseFile, {
-  define: function(db, models, next) {
-    models.vote = db.define("vote", {
-      clipID: String,
-      vote: Number
-    });
-    models.vote.getVotes = function(clipID, value, callback) {
-      this.find({ clipID: clipID, vote: value }, callback);
-    };
-    models.vote.setVote = function(clipID, value, callback) {
-      this.create([{ clipID: clipID, vote: value }], callback);
-    };
-
-    db.sync(function(err) {
-      if (err) throw err;
-      console.log("Database Synced");
-    });
-    next();
-  }
-}));
+var vote = function(req, value, fn) {
+  var result = { success: false };
+  req.models.vote.getVote(req.params.id, req.sessionID, function(err, voteArray) {
+    var currentVote = voteArray[0];
+    if (typeof(currentVote) !== "undefined") {
+      if (currentVote.value == value) {
+        fn(false);
+      } else {
+        currentVote.save({ value: value }, function(err) {
+          fn(true);
+        });
+      }
+    } else {
+      req.models.vote.setVote(req.params.id, value, req.sessionID, function(err, vote) {
+        if (err) {
+          fn(false);
+        } else {
+          fn(true);
+        }
+      });
+    }
+  });
+}
 
 
 // Index
@@ -63,44 +87,45 @@ app.get("/", function(req, res) {
 });
 
 
-// Clips
-app.get("/clips", function(req, res) {
-
-});
-
-app.get("/clips/:id", function(req, res) {
-  res.type("text/json");
+// Vote
+app.get("/clips/:id/vote", function(req, res) {
+  req.models.vote.getVote(req.params.id, req.sessionID, function(err, voteArray) {
+    var result = { value: 0 };
+    var vote = voteArray[0];
+    if (typeof(vote) !== "undefined") {
+      result.value = vote.value;
+    }
+    res.json(result);
+  });
 });
 
 
 // Upvotes
 app.get("/clips/:id/upvote", function(req, res) {
-  req.models.vote.getVotes(req.params.id, 1, function(err, upvotes) {
+  req.models.vote.getAllVotes(req.params.id, 1, function(err, upvotes) {
     numOfUpvotes = { upvotes: upvotes.length }
     res.json(numOfUpvotes);
   });
 });
 
 app.post("/clips/:id/upvote", function(req, res) {
-  req.models.vote.setVote(req.params.id, 1, function(err, upvotes) {
-    var upvote = upvotes[0];
-    res.json(upvote);
+  vote(req, 1, function(result) {
+    res.json({ success: result });
   });
 });
 
 
 // Downvotes
 app.get("/clips/:id/downvote", function(req, res) {
-  req.models.vote.getVotes(req.params.id, -1, function(err, downvotes) {
+  req.models.vote.getAllVotes(req.params.id, -1, function(err, downvotes) {
     numOfDownvotes = { downvotes: downvotes.length }
     res.json(numOfDownvotes);
   });
 });
 
 app.post("/clips/:id/downvote", function(req, res) {
-  req.models.vote.setVote(req.params.id, -1, function(err, downvotes) {
-    var downvote = downvotes[0];
-    res.json(downvote);
+  vote(req, -1, function(result) {
+    res.json({ success: result });
   });
 });
 
@@ -112,5 +137,5 @@ app.get("*", function(req, res) {
 
 
 // Start server
-app.listen(port, address);
-console.log("Server running at %s:%d", address, port);
+app.listen(app.get("port"));
+console.log("Server running on port %s", app.get("port"));
